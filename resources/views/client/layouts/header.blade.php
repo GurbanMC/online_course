82fbc1f97aa73f4b1139cd8c5e5b3648fe30dfcf
Your Name <you@example.com>
2023-02-09 07:16:39 +0500

    3.7

--- resources/views/client/layouts/header.blade.php
+++ resources/views/client/layouts/header.blade.php
@@ -1,5 +1,5 @@
 <header class="navbar navbar-dark sticky-top flex-md-nowrap p-0 shadow" style="background-color: #1B2431">
-    <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="{{ route('admin.dashboard') }}">@lang('app.app-name')
+    <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="{{ route('client.home') }}">@lang('app.app-name')
         <i class="bi-x-diamond-fill"></i>
     </a>
     <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
--- resources/views/client/layouts/sidebar.blade.php
+++ resources/views/client/layouts/sidebar.blade.php
@@ -2,44 +2,22 @@
     <div class="position-sticky py-2 sidebar-sticky">
         <ul class="nav flex-column">
             <li class="nav-item">
-                <a class="nav-link link-light text-secondary" href="{{ route('admin.dashboard') }}">
-                    <i class="bi-grid-1x2-fill text-secondary me-1"></i> @lang('app.dashboard')
+                <a class="nav-link link-light text-secondary" href="{{ route('client.home.index') }}">
+                    <i class="bi-grid-1x2-fill text-secondary me-1"></i> @lang('app.home')
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link link-light text-secondary" href="{{ route('admin.customers.index') }}">
-                    <i class="bi-people-fill text-secondary me-1"></i> @lang('app.customers')
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link link-light text-secondary" href="{{ route('admin.verifications.index') }}">
-                    <i class="bi-shield-fill-check text-secondary me-1"></i> @lang('app.verifications')
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link link-light text-secondary" href="{{ route('admin.courses.index') }}">
+                <a class="nav-link link-light text-secondary" href="{{ route('client.courses.index') }}">
                     <i class="bi-filetype-mp4 text-secondary me-1"></i> @lang('app.courses')
                 </a>
             </li>
             @can('categories')
                 <li class="nav-item">
-                    <a class="nav-link link-light text-secondary" href="{{ route('admin.categories.index') }}">
+                    <a class="nav-link link-light text-secondary" href="{{ route('client.categories.index') }}">
                         <i class="bi-grid-fill text-secondary me-1"></i> @lang('app.categories')
                     </a>
                 </li>
             @endcan
-            @can('attributes')
-                <li class="nav-item">
-                    <a class="nav-link link-light text-secondary" href="{{ route('admin.attributes.index') }}">
-                        <i class="bi-palette-fill text-secondary me-1"></i> @lang('app.attributes')
-                    </a>
-                </li>
-            @endcan
-            <li class="nav-item">
-                <a class="nav-link link-light text-secondary" href="{{ route('admin.users.index') }}">
-                    <i class="bi-people-fill text-secondary me-1"></i> @lang('app.users')
-                </a>
-            </li>
         </ul>
     </div>
 </nav>
